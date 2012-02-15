666942b2190e9fb73043cbd0a0a9a56c3fb70655
Jason Hamm <jhamm.business@gmail.com>
2012-02-15 06:35:07 -0500

    Added contact page

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -37,4 +37,16 @@ describe "StaticPages" do
       page.should have_selector('title', :text => 'About')
     end
   end
+
+  describe "Contact page" do 
+    it "should have the content 'Contact'" do
+      visit '/static_pages/contact'
+      page.should have_content('Contact')
+    end
+
+    it "should have the title 'Contact'" do
+      visit '/static_pages/contact'
+      page.should have_selector('title', :text => 'Contact')
+    end
+  end
 end

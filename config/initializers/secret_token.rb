b0a1a2950f95b17c422c8271a8434c97e3922dfc
Jason Hamm <jhamm.business@gmail.com>
2012-02-15 05:37:55 -0500

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Yamba::Application.config.secret_token = '7aa8449ee219f33e5d37f8ebac66a37feb9eddf2aa89fafe812876d96dc146a84d4eaaf0f09b9e3c5947b85c3480fa56b1e1450e280f1634591ffe3d9ef0fed2'
